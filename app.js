3d8c6d03adde7ee2556ad8df31be37cf67e587cb
Trent Townsend <trent.townsend2@gmail.com>
2022-06-19 19:56:51 +1000

    Add scoring functionality

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
+const playerOneButton = document.querySelector("#player-1");
+const playerTwoButton = document.querySelector("#player-2");
+const resetButton = document.querySelector("#reset");
+let playerOneScore = document.querySelector("#player1-score");
+let playerTwoScore = document.querySelector("#player2-score");
+let currentScoreOne = parseInt(playerOneScore.innerText);
+let currentScoreTwo = parseInt(playerTwoScore.innerText);
+let MaxScore = document.querySelector("#winning-score-select");
+
+console.log(MaxScore);
 
 
+
+
+
+playerOneButton.addEventListener('click', function () {
+    if (parseInt(MaxScore.value) === 0) {
+        alert("Please select the winning score before playing");
+    }
+    else {
+        currentScoreOne += 1;
+        playerOneScore.innerText = currentScoreOne;
+    }
+})
+
+playerTwoButton.addEventListener('click', function () {
+    if (parseInt(MaxScore.value) === 0) {
+        alert("Please select the winning score before playing");
+    }
+    else {
+        currentScoreTwo += 1;
+        playerTwoScore.innerText = currentScoreTwo;
+    }
+})
+
+resetButton.addEventListener('click', function () {
+    playerOneScore.innerText = "0";
+    playerTwoScore.innerText = "0"; 
+    currentScoreOne = 0;
+    currentScoreTwo = 0;
+})
