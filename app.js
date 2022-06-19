e0704375652811f5fbc030cc8abfda641a139e9a
Trent Townsend <trent.townsend2@gmail.com>
2022-06-19 21:54:24 +1000

    Add announcement of deuce when reached

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const playerOneButton = document.querySelector("#player-1");
 const playerTwoButton = document.querySelector("#player-2");
 const resetButton = document.querySelector("#reset");
+const deuce = document.querySelector("#deuce");
 let playerOneScore = document.querySelector("#player1-score");
 let playerTwoScore = document.querySelector("#player2-score");
 let currentScoreOne = parseInt(playerOneScore.innerText);
 let currentScoreTwo = parseInt(playerTwoScore.innerText);
 let maxScore = document.querySelector("#winning-score-select");
 
+
+function checkDeuce(currentScoreOne, currentScoreTwo, maxScore) {
+    if ((currentScoreOne >= maxScore.value || currentScoreTwo >= maxScore.value) && currentScoreOne === currentScoreTwo) {
+        deuce.style.visibility = 'visible';
+    }
+    else {
+        deuce.style.visibility = 'hidden';
+    }
+}
 
 playerOneButton.addEventListener('click', function () {
     if (parseInt(maxScore.value) === 0) {
@@ -15,6 +25,7 @@ playerOneButton.addEventListener('click', function () {
     else if ((currentScoreOne < maxScore.value && currentScoreTwo < maxScore.value) || ((Math.abs(currentScoreOne - currentScoreTwo < 2) && (Math.abs(currentScoreTwo - currentScoreOne < 2))))) {
         currentScoreOne += 1;
         playerOneScore.innerText = currentScoreOne;
+        checkDeuce(currentScoreOne, currentScoreTwo, maxScore)
     }
 })
 
@@ -25,13 +36,14 @@ playerTwoButton.addEventListener('click', function () {
     else if ((currentScoreTwo < maxScore.value && currentScoreOne < maxScore.value) || ((Math.abs(currentScoreTwo - currentScoreOne < 2)) && (Math.abs(currentScoreOne - currentScoreTwo < 2)))) {
         currentScoreTwo += 1;
         playerTwoScore.innerText = currentScoreTwo;
+        checkDeuce(currentScoreOne, currentScoreTwo, maxScore)
     }
 })
-
 
 resetButton.addEventListener('click', function () {
     playerOneScore.innerText = "0";
     playerTwoScore.innerText = "0";
     currentScoreOne = 0;
     currentScoreTwo = 0;
+    deuce.style.visibility = "hidden";
 })
